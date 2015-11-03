48d4448a1af3190d34264302861fff6357847ecb
annahuffman147 <annahuffman.plc@gmail.com>
2015-11-03 11:51:19 -0700

    thru get by id and tag

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ console.log('sanity check!')
 
 var firstParagraph = document.getElementById('main');
 console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements)
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement);
+
+//lists all li in an array
+var allListItems = document.getElementsByTagName('li');
+console.log(allListItems);
+
+//lists all li individually
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i]);
+}
