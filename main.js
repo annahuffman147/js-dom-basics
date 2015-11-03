2b2df005c313dfdecf2b475b361cc22070d405c5
annahuffman147 <annahuffman.plc@gmail.com>
2015-11-03 12:46:52 -0700

    with manipulation

--- main.js
+++ main.js
@@ -43,3 +43,41 @@ console.log(firstOddElement);
 //querySelectorAll
 var oddAndEvenElements = document.querySelectorAll('.odd, .even');
 console.log(oddAndEvenElements);
+
+//traversing the DOM with child, parent, sibling
+  var childNodes = document.body.childNodes
+  console.log(childNodes);
+
+//altering the content
+for (var i=0; i<allListItems.length; i++) {
+  console.log(allListItems[i].innerText);
+};
+
+for (var i=0; i<allListItems.length; i++) {
+  console.log(allListItems[i].innerText=i);
+}
+console.log(allListItems);
+
+
+//Manipulation
+document.getElementById('main').style.backgroundColor = 'goldenrod';
+
+var propertyTest = document.getElementById('main')
+console.log(propertyTest);
+
+//createElement
+var li = document.createElement('li');
+console.log(li);
+
+var newElement = li.innerText = 'another list item';
+console.log(newElement);
+console.log(li);
+
+getSingleElement.appendChild(li);
+console.log(allListItems);
+
+var anotherListElement = document.createElement('li');
+anotherListElement.innerText = 'prepend';
+console.log(anotherListElement);
+getSingleElement.insertBefore(anotherListElement, getSingleElement.firstChild);
+console.log(allListItems);
